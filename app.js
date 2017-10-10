408dde21c0d18d3e8a95b1ac92b4c519fc01c2df
Justin Lee <leejwg.3@gmail.com>
2017-10-09 14:13:08 -1000

    installed method override lib and switched var on server port

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ console.log('shes linked');
 
 const express = require('express');
 const port = process.env.PORT || 8080;
+const methodOverride = require('method-override');
 const app = express();
 
+
 app.get('/', (req, res) => {
-      res.send('smoke test');
-    });
+  res.send('smoke test');
+});
 
 
 
 
-app.listen(PORT, () => {
-  console.log("server listening on: ${PORT}");
+app.listen(port, () => {
+  console.log(`server listening on: ${port}`);
 });
