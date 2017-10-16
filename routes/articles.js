c25ce74df69fac3708f8b3fc0eb9fbae44d4c142
Justin Lee <leejwg.3@gmail.com>
2017-10-16 12:06:43 -1000

    adding more basics before test

--- routes/articles.js
+++ routes/articles.js
@@ -1,1 +1,18 @@
 //jshint esversion:6
+
+// Articles routes - required 
+const express = require('express');
+const app = express();
+
+
+
+//Article routes 
+
+app.get('/', (req, res) => {
+  res.render('/');
+  res.send('CHecking page');
+});
+
+app.get('/main', (req, res) => {
+  res.render('/main');
+});
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+const port = process.env.PORT || 3000;
 const express = require('express');
 const fs = require('fileSystem');
 const exhbs = require('express-handlebars');
@@ -7,3 +7,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const articles = require('./routes/articles');
 const products = require('/routes/products');
+let methodOverride = require('methodOverride');
+
+
+// What app will use 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+
+//port server end 
+app.listen(port, () => {
+  console.log(`server listening on: ${port}`);
+});
