aa4bce967fdb6f83bd7fe1b3117c70fde3cee6bf
Justin Lee <leejwg.3@gmail.com>
2017-10-16 11:56:03 -1000

    added main consts in server.js file

--- server/server.js
+++ server/server.js
@@ -1,2 +1,9 @@
 //jshint esversion:6
 
+const express = require('express');
+const fs = require('fileSystem');
+const exhbs = require('express-handlebars');
+const app = express();
+const bodyParser = require('body-parser');
+const articles = require('./routes/articles');
+const products = require('/routes/products');
